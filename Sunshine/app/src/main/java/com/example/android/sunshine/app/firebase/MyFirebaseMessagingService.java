73211ffaa2f3a78a38600d5f66b054a2306a56da
vikasdesale <vikas.desaleu@gmail.com>
2017-03-14 23:44:59 +0530

    strings_xml_changed

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/firebase/MyFirebaseMessagingService.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/firebase/MyFirebaseMessagingService.java
@@ -46,7 +46,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Time to unparcel the bundle!
         if (!data.isEmpty()) {
             // TODO: gcm_default sender ID comes from the API console
-            String senderId = getString(R.string.gcm_defaultSenderId);
+            String senderId = getString(R.string.gcm_defaultSenderId1);
             if (senderId.length() == 0) {
                 Toast.makeText(this, "SenderID string needs to be set", Toast.LENGTH_LONG).show();
             }
